ebe66a02f6db8fc558754e0e156f2fc6517f6b34
Pedro Luna <79716423+pedrolunasr@users.noreply.github.com>
2025-08-20 12:15:22 -0300

    Delete wrong cypress directory

--- cypress/support/Pages/login/elements.js
+++
@@ -1,9 +0,0 @@
-// Elementos da pagina de login
-export const ELEMENTS ={
-    url: 'https://www.saucedemo.com/v1/',
-    campoUsername: "#user-name",
-    campoPassword: "#password",
-    botaoLogin: "#login-button",
-    imgSwagLabs: '.login_logo',
-    msgErro: '[data-test="error"]'
-}
